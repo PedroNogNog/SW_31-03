a729b805a11af4b488aa19a0326d40a33914abf7
PedroNogNog <pedronognogsilva@gmail.com>
2025-03-31 14:36:24 -0300

    Update index.js

--- SW/index.js
+++ SW/index.js
@@ -1,4 +1,10 @@
 import { Titular, buscarTitulares } from "./model/Titular.js";
+import { Conta } from "./model/Conta.js";
 
 let titulares = buscarTitulares()
-console.log(titulares)
+
+let c1 = new Conta(500, 1234, 543, 2598, titulares[0])
+let c2 = new Conta(400, 12345, 5432, 25987, titulares[1])
+let c3 = new Conta(300, 123456, 54321, 259876, titulares[2])
+
+let contas = [c1, c2, c3]
